0a8d9489c18e8684b110900902eee3ed1d4e974a
Ethan Lin <ethan.lin.05@gmail.com>
2013-03-14 18:57:16 -0500

    First push
    Inital files and folders.

---
+++ Mortgage Calculator Solution/Mortgage Calculator/Main Form.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.lblLoanAmt = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(12, 201)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(460, 249)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'lblLoanAmt
+        '
+        Me.lblLoanAmt.Location = New System.Drawing.Point(12, 13)
+        Me.lblLoanAmt.Name = "lblLoanAmt"
+        Me.lblLoanAmt.Size = New System.Drawing.Size(50, 25)
+        Me.lblLoanAmt.TabIndex = 1
+        Me.lblLoanAmt.Text = "Loan"
+        Me.lblLoanAmt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 462)
+        Me.Controls.Add(Me.lblLoanAmt)
+        Me.Controls.Add(Me.ListView1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mortgage Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents lblLoanAmt As System.Windows.Forms.Label
+
+End Class
